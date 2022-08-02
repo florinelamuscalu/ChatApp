58318f5b7ed1f23c328d11de6b5d906f6d77a96c
Flori <62910673+florinelamuscalu@users.noreply.github.com>
2022-08-03 01:04:18 +0300

    added bottom bar

---
+++ README.md
@@ -0,0 +1,10 @@
+# Chat App
+
+## Getting Started
+
+In this project I want to deploy a chat app. At the moment I created the bottom bar.<br><br>
+
+<p float="left">
+<img src = 'assets/readme/ss1.png' whidth = 100 height = 300>
+</p><br><br>
+
---
+++ lib/chatapp_page.dart
@@ -0,0 +1,188 @@
+import 'package:flutter/material.dart';
+
+class chatApp extends StatefulWidget {
+  const chatApp({Key? key}) : super(key: key);
+
+  @override
+  State<chatApp> createState() => _chatAppState();
+}
+
+class _chatAppState extends State<chatApp> {
+
+  int _selectedIndex = 0;
+
+  final screen =[
+    const Center(child: Text('0'),),
+    const Center(child: Text('1'),),
+    const Center(child: Text('2'),),
+    const Center(child: Text('3'),),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: screen[_selectedIndex],
+      bottomNavigationBar: Container(
+      decoration: const BoxDecoration(
+          gradient: LinearGradient(colors: [Colors.purple, Colors.red])),
+      child: BottomAppBar(
+        elevation: 0,
+        color: Colors.transparent,
+        child: SizedBox(
+          height: 70,
+          width: MediaQuery.of(context).size.width,
+          child: Padding(
+              padding: const EdgeInsets.only(left: 25.0, right: 25.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  IconButtomBar(
+                      text: 'Message',
+                      icon: Icons.message_outlined,
+                      selected: _selectedIndex == 0, // first page is 0 (messenger)
+                      onPressed: () {
+                        setState(() {
+                          _selectedIndex = 0;
+                        });
+                      }),
+                      IconButtomBar(
+                      text: 'Appointments',
+                      icon: Icons.notes_outlined,
+                      selected: _selectedIndex == 1, // first page is 0 (messenger)
+                      onPressed: () {
+                        setState(() {
+                          _selectedIndex = 1;
+                        });
+                      }),
+                      IconButtomBar(
+                      text: 'Recipe',
+                      icon: Icons.newspaper_outlined,
+                      selected: _selectedIndex == 2, // first page is 0 (messenger)
+                      onPressed: () {
+                        setState(() {
+                          _selectedIndex = 2;
+                        });
+                      }),
+                      IconButtomBar(
+                      text: 'Medical History',
+                      icon: Icons.supervisor_account_outlined,
+                      selected: _selectedIndex == 3, // first page is 0 (messenger)
+                      onPressed: () {
+                        setState(() {
+                          _selectedIndex = 3;
+                        });
+                      }),
+                ],
+              )),
+        ),
+      ),
+    )
+    );
+  }
+}
+/*
+class BottomNavigationBarIC extends StatefulWidget {
+  const BottomNavigationBarIC({Key? key}) : super(key: key);
+
+  @override
+  State<BottomNavigationBarIC> createState() => _BottomNavigationBarICState();
+}
+
+class _BottomNavigationBarICState extends State<BottomNavigationBarIC> {
+
+  int _selectedIndex = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+          gradient: LinearGradient(colors: [Colors.purple, Colors.red])),
+      child: BottomAppBar(
+        elevation: 0,
+        color: Colors.transparent,
+        child: SizedBox(
+          height: 70,
+          width: MediaQuery.of(context).size.width,
+          child: Padding(
+              padding: const EdgeInsets.only(left: 25.0, right: 25.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  IconButtomBar(
+                      text: 'Message',
+                      icon: Icons.message_outlined,
+                      selected: _selectedIndex == 0, // first page is 0 (messenger)
+                      onPressed: () {
+                        setState(() {
+                          _selectedIndex = 0;
+                        });
+                      }),
+                      IconButtomBar(
+                      text: 'Appointments',
+                      icon: Icons.notes_outlined,
+                      selected: _selectedIndex == 1, // first page is 0 (messenger)
+                      onPressed: () {
+                        setState(() {
+                          _selectedIndex = 1;
+                        });
+                      }),
+                      IconButtomBar(
+                      text: 'Recipe',
+                      icon: Icons.newspaper_outlined,
+                      selected: _selectedIndex == 2, // first page is 0 (messenger)
+                      onPressed: () {
+                        setState(() {
+                          _selectedIndex = 2;
+                        });
+                      }),
+                      IconButtomBar(
+                      text: 'Medical History',
+                      icon: Icons.supervisor_account_outlined,
+                      selected: _selectedIndex == 3, // first page is 0 (messenger)
+                      onPressed: () {
+                        setState(() {
+                          _selectedIndex = 3;
+                        });
+                      }),
+                ],
+              )),
+        ),
+      ),
+    );
+  }
+}
+*/
+class IconButtomBar extends StatelessWidget {
+  final String text;
+  final IconData icon;
+  final bool selected;
+  final Function() onPressed;
+
+  const IconButtomBar(
+      {required this.text,
+      required this.icon,
+      required this.selected,
+      required this.onPressed,
+      Key? key})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        IconButton(
+          onPressed: onPressed,
+          icon:
+              Icon(icon, size: 25, color: selected ? Colors.white : Colors.black),
+        ),
+        Text(
+          text,
+          style: TextStyle(
+              fontSize: 12,
+              height: 1,
+              color: selected ? Colors.white : Colors.black),
+        )
+      ],
+    );
+  }
+}
