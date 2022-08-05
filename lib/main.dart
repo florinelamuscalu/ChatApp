1b0a9da385a6d7ba99c59bcd86f6cb9f8715fd79
Flori <62910673+florinelamuscalu@users.noreply.github.com>
2022-08-05 22:25:19 +0300

    add login google

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,10 @@
+import 'package:chatapp/allConstants/app_constants.dart';
+import 'package:chatapp/allProvider/auth_provider.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'chatapp_page.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:provider/provider.dart';
+//import 'chatapp_page.dart';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -22,14 +27,33 @@ class MyApp extends StatelessWidget {
   final FirebaseFirestore firebaseFirestore = FirebaseFirestore.instance;
   final FirebaseStorage firebaseStorage = FirebaseStorage.instance;
 
+
   MyApp({required this.prefs});
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: chatApp()
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => AuthProvider(
+          firebaseAuth: FirebaseAuth.instance,
+          googleSignIn: GoogleSignIn(),
+          prefs: this.prefs,
+          firebaseFirestore: this.firebaseFirestore
+        ))
+      ],
+      child: MaterialApp(
+        title: AppConstants.appTitle,
+        theme: ThemeData(
+          primaryColor:  Colors.black,
+        ),
+        home: const Scaffold(),
+        debugShowCheckedModeBanner: false,
+      ),
     );
+    // return const MaterialApp(
+    //   debugShowCheckedModeBanner: false,
+    //   home: chatApp()
+    // );
   }
   }
