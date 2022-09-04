47813fc27a23441fa2207e18668c6b084644baa2
Flori <62910673+florinelamuscalu@users.noreply.github.com>
2022-09-04 03:26:31 +0300

    update

--- lib/allScreens/spalsh_page.dart
+++ lib/allScreens/spalsh_page.dart
@@ -18,7 +18,7 @@ class _SplashPageState extends State<SplashPage> {
   @override
   void initState() {
     super.initState();
-    Future.delayed(const Duration(seconds: 5), () {
+    Future.delayed(const Duration(seconds: 1), () {
       checkSignedIn();
     });
   }
